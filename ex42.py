808dee873d84b00e1ee8741e197e93a122832b4c
changryeul <crlee123@gmail.com>
2022-02-05 23:19:55 +0900

    Add Mess[aaaa]

---
+++ ex42.py
@@ -0,0 +1,29 @@
+class animal(object):
+    pass
+    
+class dog( animal):
+    
+    def __init__(self,name):
+        self.name= name
+
+class cat(animal):
+    def __init__(self,name):
+        self.name=name
+
+class person( object):
+    def __init__( self,name)
+        self.name = name
+        self.pet  = None
+        
+class laber( person):
+    def __init__(self,name,salery)
+        super( person
+    
+ main 
+ dlll
+ 
+ dfsfsafa
+ 
+ 
+ def mal(a, b):
+    return a*b
